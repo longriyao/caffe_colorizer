a017e4d04a185fc23c694c98186a115c98846ed8
Your Name <you@example.com>
2016-12-18 09:33:12 +0800

    update

--- train.sh
+++ train.sh
@@ -1,2 +1,3 @@
-../caffe-master/build/tools/caffe train --solver model/solver.prototxt
+../caffe-master/build/tools/caffe train --solver model/solver.prototxt 
+#--weights state/gan_iter_50000.caffemodel
 #~/Soft/caffe-master/build/tools/caffe train --solver model/solver.prototxt
